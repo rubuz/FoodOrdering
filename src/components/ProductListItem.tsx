7f7a7fc6bce394d5bbda401ede634024e8ca0485
Matjaz Lovenjak <rubuz23@gmail.com>
2024-04-25 21:14:00 +0200

    Refactor TabOneScreen to MenuScreen and use ProductListItem component

--- src/components/ProductListItem.tsx
+++ src/components/ProductListItem.tsx
@@ -1,11 +1,22 @@
 import Colors from "@/src/constants/Colors";
 import { Image, StyleSheet, Text, View } from "react-native";
 import products from "@/assets/data/products";
+import { Product } from "../types";
 
-const ProductListItem = ({ product }) => {
+export const defaultPizzaImage =
+  "https://cdn.pixabay.com/photo/2017/12/09/08/18/pizza-3007395_960_720.jpg";
+
+type ProductListItemProps = {
+  product: Product;
+};
+
+const ProductListItem = ({ product }: ProductListItemProps) => {
   return (
     <View style={styles.container}>
-      <Image style={styles.image} source={{ uri: product.image }} />
+      <Image
+        style={styles.image}
+        source={{ uri: product.image || defaultPizzaImage }}
+      />
       <Text style={styles.title}>{product.name}</Text>
       <Text style={styles.price}>${product.price}</Text>
     </View>
